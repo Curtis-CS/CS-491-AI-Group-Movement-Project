5d108436a3445be98038fb6a2e80c490bec4c7d6
Cody Jackson <cody@krselectric.com>
2023-03-03 15:34:03 -0800

    Creates basic main menu + MenuMgr

---
+++ Assets/MenuMgr.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuMgr : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LoadCircular(int obstacles)
+    {
+
+    }
+
+    public void LoadRectangular(int obstacles)
+    {
+
+    }
+
+    public void LoadAStar()
+    {
+
+    }
+
+    public void LoadOffice()
+    {
+
+    }
+}
